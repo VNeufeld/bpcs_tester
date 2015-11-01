e83c4236c35002337be4f47b62510288961f16b6
ValeriN <valeri.neufeld@arcor.de>
2015-11-01 18:05:05 +0100

    initial

---
+++ bpcs-tester/src/main/java/com/bpcs/bpcs_tester/App.java
@@ -0,0 +1,13 @@
+package com.bpcs.bpcs_tester;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
